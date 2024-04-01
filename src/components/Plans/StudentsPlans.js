f26ef388bee07c792d6003cdf907a83eeaa927d3
mymediequip <mymediequipp@gmail.com>
2024-04-01 12:16:07 +0530

    working on studentplans

--- src/components/Plans/StudentsPlans.js
+++ src/components/Plans/StudentsPlans.js
@@ -8,7 +8,7 @@ const StudentsPlans = () => {
         Choose the plan that's right for your career growth
       </h1>
       <div className="flex items-center justify-center text-center">
-        <div className="border bg-white p-4 rounded-md">
+        <div className="border bg-white p-4 rounded-md mt-5">
           <h4 className="pt-4 pb-4 text-lg font-bold">Single Course program</h4>
           <p className="pt-4 pb-4">
             Learn a single topic or skill and earn a credential
@@ -28,17 +28,53 @@ const StudentsPlans = () => {
 
           <p className="pt-4 pb-4">Earn a certificate upon completion</p>
         </div>
-        <div className="border-none bg-white p-4 rounded-md">
+        <div className="border-2 border-blue-600 bg-blue-600 rounded-lg">
+          <h4 className="text-white">Most Popular</h4>
+          <div className="border-none bg-white p-4 rounded-md">
+            <h4 className="pt-4 pb-4 text-lg font-bold">
+              Multiple Course program
+            </h4>
+            <h3 className="pt-4 pb-4 text-4xl font-semibold"> ₹4,876 /month</h3>
+            <JoinFreeBtn
+              title={"Start 7-day free trial"}
+              bgColor={"bg-blue-600"}
+              color={"text-white"}
+              className="px-[10px]"
+            />
+            <p>Cancel anytime</p>
+            <hr className="border"></hr>
+            <p className="pt-4 pb-4">
+              Access 7,000+ courses and Specializations from 170+ leading
+              companies and universities
+            </p>
+            <hr className="border"></hr>
+            <p className="pt-4 pb-4">Earn unlimited certificates</p>
+            <hr className="border"></hr>
+            <p>
+              Learn job-relevant skills and tools with 1,000+ applied projects
+              and hands-on labs from industry experts
+            </p>
+            <hr className="border"></hr>
+            <p>
+              Choose from more than 15 Professional Certificate programs from
+              industry leaders like Google, Facebook, and more
+            </p>
+          </div>
+        </div>
+        <div className="border bg-white p-4 rounded-md mt-5">
           <h4 className="pt-4 pb-4 text-lg font-bold">
             Multiple Course program
           </h4>
-          <h3 className="pt-4 pb-4 text-4xl font-semibold"> ₹4,876 /month</h3>
-          <JoinFreeBtn
-            title={"Start 7-day free trial"}
-            bgColor={"bg-blue-600"}
-            color={"text-white"}
-            className="px-[10px]"
-          />
+          <p className="pt-4 pb-4">
+            Learn a single topic or skill and earn a credential
+          </p>
+          <h3 className="pt-4 pb-4 text-4xl font-semibold">
+            {" "}
+            ₹4,876 - ₹6,367/month
+          </h3>
+          <p className="pt-4 pb-4">
+            Visit an individual course or Specialization page to purchase.
+          </p>
           <hr className="border"></hr>
           <p className="pt-4 pb-4">
             Access all courses within the learning program
